f507fb4d45e3794e976bf45e1002a640833f4956
Andrew Ernest <anernest@ratesresearch.org>
2019-11-07 09:58:34 -0600

    updated bootstrap.sh to pull appropriate repos

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # exit if anything returns failure
+
 set -e
+yum -y update --disablerepo=epel\*
+yum -y install epel-release
+rpm -Uvh http://yum.postgresql.org/9.6/redhat/rhel-6-x86_64/pgdg-redhat96-9.6-3.noarch.rpm
 cd /etc/yum.repos.d/
 wget  https://yum.boundlessps.com/geoshape.repo
 yum -y install geoshape geoshape-geoserver elasticsearch postgis-postgresql95 rabbitmq-server-3.6.1
